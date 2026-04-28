736d972b86f79d263ebe87df385bf1f3767fe05c
dajebbar <dajebbar@gmx.fr>
2026-04-28 13:47:53 +0100

    Menus dict

---
+++ 03-conditionals/restaurent_billing.py
@@ -0,0 +1,22 @@
+menus = {
+    "classic":{
+        "café": 1.50,
+        "croissant": 1.20,
+        "jus d'orange": 2.00
+    },
+
+    "energy":{
+        "thé": 1.50,
+        "pain complet": 0.80,
+        "beurre": 0.50,
+        "confiture": 0.60,
+        "banane": 0.90
+    },
+
+    "gourmand":{
+        "chocolat chaud": 2.20,
+        "pain au chocolat": 1.40,
+        "tartine beurre-confiture": 1.10,
+        "kiwi": 0.80
+    }
+}
