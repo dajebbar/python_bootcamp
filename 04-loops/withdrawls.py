fb59b26c5ef45675f18dfa0e2934d1a79a795421
dajebbar <dajebbar@gmx.fr>
2026-06-10 09:59:49 +0100

    widrawls

--- 04-loops/withdrawls.py
+++ 04-loops/withdrawls.py
@@ -1,4 +1,20 @@
 def widrawls(balance, withlst):
-    print("hello")
+    msg = ""
+    msg_lst = []
+    for item in withlst:
+        if item <= balance:
+            balance -= item
+            msg = f"Withdrawn: {balance}"
+            msg_lst.append(msg)
+        else:
+            msg = f"Insufficient funds for requested amount: {balance}"
+            msg_lst.append(msg)
+        
+    msg = f"Remaining Balance: {balance}"
+    msg_lst.append(msg)
+    return msg_lst
+            
+            
 
-widrawls(23, [21,34])
+res = widrawls(1000, [34,50,15,8])
+print(res)
