089ca7d7725ea2bf3edfe1406bb6cefac7418bc5
dajebbar <dajebbar@gmx.fr>
2026-06-16 13:22:15 +0100

    del en

--- 10-OOPs/encryp_descriptor.py
+++ 10-OOPs/encryp_descriptor.py
@@ -1,25 +1,3 @@
-# Contexte : Vous voulez réutiliser une validation de chaîne de caractères partout. 
-# Créez un descripteur NonEmptyString qui garantit qu’un attribut est une chaîne 
-# non vide (après suppression des espaces).
-
-# Consignes :
-
-# Créez le descripteur NonEmptyString :
-
-# Il reçoit un nom dans __init__ (pour stocker la clé dans __dict__).
-
-# Dans __set__, vérifiez que value est une instance de str et 
-# que value.strip() n’est pas vide. Sinon, levez ValueError avec un message explicite.
-
-# Stockez la valeur dans instance.__dict__[self._nom] (n’oubliez pas le getter).
-
-# Utilisez-le dans une classe Personne qui a deux attributs : 
-# prenom et nom, tous deux utilisant le descripteur NonEmptyString.
-
-# Ajoutez une propriété nom_complet (lecture seule) qui retourne "{prenom} {nom}".
-
-
-
 class NonEmptyString:
     def __init__(self, name):
         self._stock = name
