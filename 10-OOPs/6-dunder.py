eb17db2c31925a47c145c8c2327eafe3659974d2
dajebbar <dajebbar@gmx.fr>
2026-06-19 15:06:54 +0100

    len bool

--- 10-OOPs/6-dunder.py
+++ 10-OOPs/6-dunder.py
@@ -9,7 +9,28 @@ class Point:
     def __repr__(self):
         return f"Point({self.x}, {self.y})"
 
+class ListePersonnalisee:
+    def __init__(self, elements):
+        self.elements = elements
+    
+    def __len__(self):
+        return len(self.elements)
+    
+    def __bool__(self):
+        return any(self.elements)
+
+
+
 
 pt = Point(3,4)
 print(pt)
 print(repr(pt))
+
+l = ListePersonnalisee([0,1,3])
+print(len(l))
+print(bool(l))
+
+l2 = ListePersonnalisee([0,0,0, 0])
+print(len(l2))
+print(bool(l2))
+
