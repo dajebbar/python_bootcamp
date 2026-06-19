2b8d76ac9314a0e5265d46895044c4a192c3aa2f
dajebbar <dajebbar@gmx.fr>
2026-06-19 15:12:47 +0100

    add radd

--- 10-OOPs/6-dunder.py
+++ 10-OOPs/6-dunder.py
@@ -20,6 +20,22 @@ class ListePersonnalisee:
         return any(self.elements)
 
 
+class Vecteur:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __add__(self, autre):
+        if not isinstance(autre, Vecteur):
+            raise TypeError("Addition uniquement avec un autre Vecteur")
+        return Vecteur(self.x + autre.x, self.y + autre.y)
+    
+    def __radd__(self, autre):
+        # autre est l'opérande de gauche (ex: 5)
+        return self.__add__(autre) 
+
+    def __repr__(self):
+        return f"Vecteur({self.x}, {self.y})"
+
 
 
 pt = Point(3,4)
@@ -33,4 +49,8 @@ print(bool(l))
 l2 = ListePersonnalisee([0,0,0, 0])
 print(len(l2))
 print(bool(l2))
+
+v1 = Vecteur(1, 2)
+v2 = Vecteur(3, 4)
+v3 = v1 + v2
 
